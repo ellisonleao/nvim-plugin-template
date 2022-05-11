143fb74542b9976dac1f381f4255e208980b88e2
vinibispo <vini.bispo015@gmail.com>
2022-05-11 00:28:28 -0300

    chore(minimal_init): remove print

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,7 +1,7 @@
 local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
 local is_not_a_directory = vim.fn.isdirectory(plenary_dir) == 0
 if is_not_a_directory then
-  print(vim.fn.system({"git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir}))
+  vim.fn.system({"git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir})
 end
 
 vim.opt.rtp:append(".")
